26bba1519a0cf41630db44c4722d7f9b5459d27b
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-16 11:22:10 +0330

    setup rules

--- blog/app/Http/Controllers/api/v1/AuthenticationController.php
+++ blog/app/Http/Controllers/api/v1/AuthenticationController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+    }
 }
