1e745f438e6a9c3a4b429013816e7c1bf6ac4c32
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-16 16:14:52 +0330

    complete index method

--- blog/app/Http/Controllers/api/v1/PostController.php
+++ blog/app/Http/Controllers/api/v1/PostController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::all();
+        return response($posts, 201);
     }
 
     public function show()
