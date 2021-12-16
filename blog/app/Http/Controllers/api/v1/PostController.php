2b99e4fd2e6873a6c2e44293375d8122665560a4
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-16 16:27:51 +0330

    complete update method

--- blog/app/Http/Controllers/api/v1/PostController.php
+++ blog/app/Http/Controllers/api/v1/PostController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -20,12 +21,34 @@ class PostController extends Controller
     {
     }
 
-    public function store(StorePostRequest $request)
+    public function store(StorePostRequest $request, Post $post)
     {
+        $post->create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => Auth()->id,
+        ]);
+
+        $response = array(
+            'post' => $post
+        );
+
+        return response($response, 201);
     }
 
-    public function update(UpdatePostRequest $request)
+    public function update(UpdatePostRequest $request, Post $post)
     {
+        $post->update([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => Auth()->id,
+        ]);
+
+        $response = array(
+            'post' => $post
+        );
+
+        return response($response, 201);
     }
 
     public function destroy($id)
