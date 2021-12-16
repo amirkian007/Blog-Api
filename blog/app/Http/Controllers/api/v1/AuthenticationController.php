5f825c0aa00b388375004c7154cf45fdb0afc4fc
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-16 11:38:19 +0330

    add login method

--- blog/app/Http/Controllers/api/v1/AuthenticationController.php
+++ blog/app/Http/Controllers/api/v1/AuthenticationController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
@@ -16,6 +17,22 @@ class AuthenticationController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        $response = array(
+            'user' => $user,
+            'token' => $user->createToken('tokens')->plainTextToken,
+        );
+
+        return response($response, 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request)) {
+            return $this->error('User Not Found!', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
 
         $response = array(
             'user' => $user,
