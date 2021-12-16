9a7c9de9dfba66a3bfae06dd205d53b419700ef1
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-16 16:10:29 +0330

    add methods to post controll

--- blog/app/Http/Controllers/api/v1/PostController.php
+++ blog/app/Http/Controllers/api/v1/PostController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+    }
+
+    public function show()
+    {
+    }
+
+    public function store(StorePostRequest $request)
+    {
+    }
+
+    public function update()
+    {
+    }
 }
