c14794067cd685ca3f6407de149b5807e6ffa6df
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-16 11:32:20 +0330

    add register method

--- blog/app/Http/Controllers/api/v1/AuthenticationController.php
+++ blog/app/Http/Controllers/api/v1/AuthenticationController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
     public function register(RegisterRequest $request)
     {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $response = array(
+            'user' => $user,
+            'token' => $user->createToken('tokens')->plainTextToken,
+        );
+
+        return response($response, 201);
     }
 }
