4c67875290ff1d5903c42d1c7622f4dc8caf14e2
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-17 18:53:40 +0330

    fix some bugs in routes

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\v1\AuthenticationController;
+use App\Http\Controllers\api\v1\CommentController;
 use App\Http\Controllers\api\v1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
