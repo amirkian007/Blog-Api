2c4de634100e252486e96ed80de380515b7df5d7
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-17 18:45:39 +0330

    complete user & comment model

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -18,4 +18,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
