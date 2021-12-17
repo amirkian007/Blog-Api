c37c4e11d1357a43fc7e953f0109eaa202c329e1
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-17 11:46:13 +0330

    edit login test

--- blog/tests/Unit/LoginTest.php
+++ blog/tests/Unit/LoginTest.php
@@ -2,17 +2,26 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_can_login_with_true_credentials()
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+        $response = $this->postJson(route('login'), [
+            'email' => $user->email,
+            'password' => $user->password,
+        ]);
+        $response->assertStatus(Response::HTTP_CREATED);
     }
 }
