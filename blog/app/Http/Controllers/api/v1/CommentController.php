4382ec46900e36c8310a42c676b373d41f12e584
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-17 19:02:30 +0330

    add store method to comment controll

--- blog/app/Http/Controllers/api/v1/CommentController.php
+++ blog/app/Http/Controllers/api/v1/CommentController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCommentRequest;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
+    public function store(StoreCommentRequest $request, Post $post)
+    {
+        $comment = Comment::create([
+            'body' => $request->body,
+            'post_id' => $post->id,
+            'user_id' => Auth::user()->id,
+        ]);
 
+        return response($comment, 201);
+    }
 }
