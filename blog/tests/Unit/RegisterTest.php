4a39e7fcb2cea66fc19f96b8ff7ce1351b65ce99
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-12-17 10:54:17 +0330

    add one test to register

--- blog/tests/Unit/RegisterTest.php
+++ blog/tests/Unit/RegisterTest.php
@@ -2,17 +2,30 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class RegisterTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_can_register()
     {
-        $this->assertTrue(true);
+        $user = [
+            'name' => 'bgn',
+            'email' => 'baghban.b@gmail.com',
+            'password' => '123456789',
+        ];
+        $this->postJson(route('register'), $user);
+        $this->assertDatabaseHas('users', [
+            'email' => $user['email'],
+        ]);
     }
+
+
 }
